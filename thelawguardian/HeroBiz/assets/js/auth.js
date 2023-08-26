dddb278e7b645b10b0b7e29136bd21afac90647c
Anishn21 <krishnaanish21@gmail.com>
2023-08-27 00:57:18 +0530

    update 2

--- thelawguardian/HeroBiz/assets/js/auth.js
+++ thelawguardian/HeroBiz/assets/js/auth.js
@@ -1,17 +1,77 @@
 function connectWallet() {
   var btn = document.getElementById("btn-auth");
-//   neologin.getAccount().then(({ address }) => {
-//     console.log(address);
-//     btn.innerHTML = address;
-//     // btn.attribute.disabled = true;
-//   });
-    const neoline = new NEOLine.Init();
-    neoline.getAccount()
-    .then(account => {console.
-        log(account)
-        var name = account.label;
-        var addr = account.address;
-        btn.innerHTML = name;
+  const neoline = new NEOLine.Init();
+  neoline
+    .getAccount()
+    .then((account) => {
+      const { address, label, isLedger } = account;
+      console.log(account);
+      swal("Success", "Wallet Successfully Connected!", "success");
+      btn.innerHTML = label;
     })
-    
+    .catch((error) => {
+      const { type, description, data } = error;
+      switch (type) {
+        case "NO_PROVIDER":
+          console.log("No provider available.");
+          swal("Info", "No provider available.", "info");
+          break;
+        case "CONNECTION_DENIED":
+          console.log(
+            "The user rejected the request to connect with your dApp"
+          );
+          swal(
+            "Error",
+            "The user rejected the request to connect with your dApp",
+            "error"
+          );
+          break;
+        default:
+          swal("Error", error, "error");
+          console.error(error);
+          break;
+      }
+    });
 }
+
+function uploadBtn() {
+    const neoline = new NEOLine.Init();
+    var btn = document.getElementById("btn-auth");
+    if(btn.innerHTML == "Get Started"){
+        neoline
+    .getAccount()
+    .then((account) => {
+      const { address, label, isLedger } = account;
+      console.log(account);
+      swal("Success", "Wallet Successfully Connected!", "success");
+      btn.innerHTML = label;
+    })
+    .catch((error) => {
+      const { type, description, data } = error;
+      switch (type) {
+        case "NO_PROVIDER":
+          console.log("No provider available.");
+          swal("Info", "No provider available.", "info");
+          break;
+        case "CONNECTION_DENIED":
+          console.log(
+            "The user rejected the request to connect with your dApp"
+          );
+          swal(
+            "Error",
+            "The user rejected the request to connect with your dApp",
+            "error"
+          );
+          break;
+        default:
+          swal("Error", error, "error");
+          console.error(error);
+          break;
+      }
+    });
+    }
+    else{
+        window.location.href = "./upload.html";
+    }
+}
+
