5ec34f22e59369c38f1819df4af5e3d02a5c9486
Anishn21 <krishnaanish21@gmail.com>
2023-08-27 12:56:20 +0530

    msg

---
+++ thelawguardian/HeroBiz/upload/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import CreateProduct from './components/create';
+// import DocumentUploader from './components/upload';
+
+function App() {
+  return (
+    <CreateProduct />
+    // <DocumentUploader />
+  );
+}
+
+export default App;
